95410123e56e8a0c428976adf8be22bc13c6f88e
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2024-10-22 17:55:45 +0300

    Initial Commit

---
+++ Objectives/ObjectivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectivesApp.swift
+//  Objectives
+//
+//  Created by Oğuzhan KERTMEN on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ObjectivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
